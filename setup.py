0ee63e1ec2e75ef132eaa3989c611e55e74a8fd4
Michel Fodje <michel.fodje@lightsource.ca>
2018-10-11 11:20:41 -0600

    Don't worry about keyboardInterrupt, version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='python-softdev',
-    version='0.2.2',
+    version='0.2.3',
     packages=['softdev'],
     scripts=['bin/softdev-startproject'],
     url='',

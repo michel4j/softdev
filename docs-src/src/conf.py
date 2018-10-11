2aae19f4eb08edd9c2fbd19c3be3a067ce134c7f
Michel Fodje <michel.fodje@lightsource.ca>
2018-10-11 15:44:22 -0600

    Rebuilt documentation

--- docs-src/src/conf.py
+++ docs-src/src/conf.py
@@ -25,9 +25,9 @@ copyright = u'2018, Michel Fodje'
 author = u'Michel Fodje'
 
 # The short X.Y version
-version = u''
+version = u'v0.2.7'
 # The full version, including alpha/beta/rc tags
-release = u'0.2.0'
+release = u'0.2.7'
 
 
 # -- General configuration ---------------------------------------------------

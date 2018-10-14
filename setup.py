22227c184692023ecd7e2f787d146c90aacebbbf
Michel Fodje <michel.fodje@lightsource.ca>
2018-10-14 12:30:22 -0600

    Fix typo in generated runIOC.py. Address conflict in generation of __init__.py and init-template.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), 'r') as 
 
 setup(
     name='python-softdev',
-    version='0.2.9',
+    version='0.2.10',
     packages=['softdev'],
     scripts=['bin/softdev-startproject'],
     url='',
